363c7600ced00ebc2ef0ecdb8524d0560d1aa6bd
Adhish Vishwakarma <adhish@gttechies2.hyd.corp.google.com>
2017-01-24 01:03:08 +0530

    Starting project

---
+++ userauth/userauth/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.contrib.auth import views
+from userlogin.forms import LoginForm
+
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('userlogin.urls')),
+    url(r'^login/$', views.login, {'template_name': 'login.html', 'authentication_form': LoginForm}, name='login'),
+    url(r'^logout/$', views.logout, {'next_page': '/login'}),  
+]
+
