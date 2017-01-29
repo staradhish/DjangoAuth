004dae3bffff9f6207e8d9cc1306692a89f68caa
rpateriya <rpateriya@rpateriya1.roam.corp.google.com>
2017-01-29 22:28:38 +0530

    raghu form

--- userauth/userlogin/urls.py
+++ userauth/userlogin/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'^register/$', views.registration, name='register'),
+    url(r'^reg/$', views.RegistrationClass.as_view()),
 ]
 
--- userauth/userlogin/views.py
+++ userauth/userlogin/views.py
@@ -1,8 +1,36 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+# import model
+
 
 @login_required(login_url="login/")
 def home(request):
-	return render(request,"home.html")
+    return render(request,"home.html")
 
+def registration(request):
+    return render(request, "registration.html")
+
+
+#-- new style
+
+from django.views import View
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+class RegistrationClass(View):
+  template_name = "registration.html"
+  
+  def get(self, request, *args, **kwargs):
+     return render(request, self.template_name)
+
+  def post(self, request, *args, **kwargs):
+   print "=="*40
+   first_name =  request.POST.get("first_name", None)
+   from blog.models import Blog 
+   b = Blog(name=first_name)
+   b.save() 
+
+   return HttpResponse("Hi yoyoyoy")
